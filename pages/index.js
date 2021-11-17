46680215def2336ad1f5a41f459f6b021e5f97ab
Brayden Kluss <brayden.lisecki@gmail.com>
2021-11-17 14:16:35 -0700

    coming soon page

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,18 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Brayden Lisecki</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="BRAYDEN LISECKI" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          coming soon
         </p>
+        <p><a href="mailto:hello@braydenlisecki.com">hello@braydenlisecki.com</a></p>
       </main>
 
-      <Footer />
     </div>
   )
 }
