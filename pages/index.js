3475ac6db95af437fee458d7164647a8637bd2b9
Brayden Kluss <brayden.lisecki@gmail.com>
2022-02-22 13:08:33 -0700

    added nav and boilerplate for home page

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,37 @@ export default function Home() {
 
   return (
     <body>
-<div>
-  <nav>this is the nav</nav>
-  <img src="https://i.imgur.com/UElJMY1.jpg" />
-  <div className="headline">
-  <h1>Brayden Lisecki</h1>
-  <h4>Guy on the Internet</h4>
-  <p>Writing about life.</p>
-  </div>
-</div>
+  <header>
+    <div className="logo">
+    <a id="logo" href="">Brayden Lisecki</a>
+    </div>
+    <nav>    
+      <ul className="nav_links">
+      <li><a href="/blog">blog</a></li>
+      <li><a href="/contact">contact</a></li>
+    </ul>
+    </nav>
+  </header>
+  <main>
+    <section>
+    <h2>Learn About Me</h2>
+    <p>I'm a writer, marketer, and thinker. </p>
+    <p>I'm currently the Marketing Project Lead for Quorum Information Systems, and I explore everything else online. </p>
+    </section>
+    <section>
+    <h2>Read My Writing</h2>
+    <p>I'm a writer, marketer, and thinker. </p>
+    <p>I'm currently the Marketing Project Lead for Quorum Information Systems, and I explore everything else online. </p>
+    </section>
+    <section>
+    <h2>Connect With Me</h2>
+    <p>hello@braydenlisecki.com</p>
+    <p>LinkedIn</p>
+    <p>Twitter</p>
+    <p>Instagram </p>
+    <p>YouTube</p>
+    </section>
+  </main>
 </body>
   )
 }
